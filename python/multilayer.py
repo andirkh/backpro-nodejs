ea4ea13442cc62fa8c049f99d2c5c85633257ab7
niltavalabs-dev <andi@niltavalabs.com>
2018-12-06 08:33:54 +0700

    multilayer neuron python

--- python/multilayer.py
+++ python/multilayer.py
@@ -12,9 +12,6 @@ for i in range(len(data)):
     inputDataset = np.append(inputDataset, np.array([data[i]["input"]]), axis=0)
     outputDataset = np.append(outputDataset, np.array([data[i]["output"]]), axis=0)
 
-# iterasi:
-epoch = 100000
-
 # inisialisasi weight random dari -1 ke 1 :
 inputShape = inputDataset.shape    # tuple baris, kolom
 outputShape = outputDataset.shape
@@ -25,7 +22,8 @@ print("outputShape")
 print(outputShape)
 """
 weight1 = 2 * np.random.random((inputShape[1], inputShape[0])) - 1 
-weight2 = 2 * np.random.random((outputShape[0], 3)) - 1
+weight3 = 2 * np.random.random((outputShape[0], 3)) - 1
+weight2 = 2 * np.random.random((18, 18)) - 1
 """
 print("weight1")
 print(weight1.shape)
@@ -39,11 +37,15 @@ def sigmoid(x, d):
     elif d == False:
         return 1/(1+np.exp(-x))
 
+epoch = 100000
+
 for j in range(epoch):
     # feed forward
     inputLayer = inputDataset
     hiddenLayer = sigmoid(np.dot(inputLayer, weight1), False)
-    outputLayer = sigmoid(np.dot(hiddenLayer, weight2), False)
+    hiddenLayer2 = sigmoid(np.dot(hiddenLayer, weight2), False)
+    outputLayer = sigmoid(np.dot(hiddenLayer, weight3), False)
+    
     """
     print("hiddenLayer")
     print(hiddenLayer.shape)
@@ -52,13 +54,16 @@ for j in range(epoch):
     """
     # propagate
     outputError = outputDataset - outputLayer
-    delta2 = outputError * sigmoid(outputLayer, True)
+    delta3 = outputError * sigmoid(outputLayer, True)
     """
     print("delta2")
     print(delta2.shape)
     """
-    print ("Err:" + str(np.mean(np.abs(outputError))))
+    print (str(j) + ". Err:" + str(np.mean(np.abs(outputError))))
     
+    hiddenError2 = delta3.dot(weight3.T)
+    delta2 = hiddenError2 * sigmoid(hiddenLayer2, True)
+
     hiddenError = delta2.dot(weight2.T)
     delta1 = hiddenError * sigmoid(hiddenLayer, True)
     """
@@ -67,6 +72,7 @@ for j in range(epoch):
     print("delta1")
     print(delta1.shape)
     """
+    weight3 += hiddenLayer2.T.dot(delta3)
     weight2 += hiddenLayer.T.dot(delta2)
     weight1 += inputLayer.T.dot(delta1)
     
